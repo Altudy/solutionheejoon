2107c423bf64149105068634f96cdec91e9c78e1
benlee73 <55429965+benlee73@users.noreply.github.com>
2019-10-02 19:49:18 +0900

    Add files via upload

---
+++ study_0924_baeksul.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	int tall[9], sum(0);
+	for (int i = 0; i < 9; i++)
+	{
+		cin >> tall[i];
+		sum += tall[i];
+	}
+	for (int i = 0; i < 8; i++)
+	{
+		for(int j = i+1; j < 9 ; j++)
+		{
+			if (sum - tall[i] - tall[j] == 100)
+			{
+				tall[i]=0;
+				tall[j]=0;
+				sort(tall, tall + 9);
+				cout << tall[2] << endl << tall[3] << endl << tall[4] << endl << tall[5] << endl << tall[6] << endl << tall[7] << endl << tall[8];
+				return 0;
+			}
+		}
+	}
+	return 0;
+}
