84b6e8faade6636083aad8d61076683056c3b8fe
benlee73 <55429965+benlee73@users.noreply.github.com>
2019-10-02 19:56:27 +0900

    Add files via upload

---
+++ study_0923_snail.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int A, B, V, day(0);
+	cin >> A >> B >> V;
+	V -= A;
+	day++;
+	if (V <= 0) {
+		cout << day;
+		return 0;
+	}
+	if (V % (A - B) == 0) {
+		V = V / (A - B);
+		day = day + V;
+	}
+	else {
+	V = V / (A - B);
+	day = day + V + 1;
+	}
+	cout << day;
+	return 0;
+}
