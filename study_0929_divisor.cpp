26219766ec0e565a514f009f3ae5172bbde2d011
benlee73 <55429965+benlee73@users.noreply.github.com>
2019-10-02 19:45:01 +0900

    Add files via upload

---
+++ study_0929_divisor.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int num, a, aMax(0), aMin(1000000);
+
+	cin >> num;
+	for (int i = 0; i < num; i++) {
+		cin >> a;
+		aMax = max(aMax, a);
+		aMin = min(aMin, a);
+	}
+	cout << aMax * aMin;
+	return 0;
+}
