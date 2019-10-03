1691c4300eff26201868a7776ff0bced5016080c
benlee73 <55429965+benlee73@users.noreply.github.com>
2019-10-04 01:25:58 +0900

    Add files via upload

---
+++ study_1004_mathisphysical.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num, ans;
+	cin >> num;
+	ans = num % 8;
+
+	switch(ans) {
+	case 0: cout << "2"; break;
+	case 1: cout << "1"; break;
+	case 2: cout << "2"; break;
+	case 3: cout << "3"; break;
+	case 4: cout << "4"; break;
+	case 5: cout << "5"; break;
+	case 6: cout << "4"; break;
+	case 7: cout << "3"; break;
+	}
+	return 0;
+}
