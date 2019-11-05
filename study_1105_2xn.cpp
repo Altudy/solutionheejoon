26cb298f21b2e2031c0063e06fc5d7d1acf34a14
benlee73 <55429965+benlee73@users.noreply.github.com>
2019-11-06 02:30:08 +0900

    Add files via upload

---
+++ study_1105_2xn.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>		// 백준 11726
+using namespace std;
+
+int main()
+{
+	int n, a[1001];
+	cin >> n;
+
+	a[1] = 1;
+	a[2] = 2;
+	for (int i = 3; i <= n; i++)
+	{
+		a[i] = (a[i - 1] + a[i - 2]) % 10007;
+	}
+	cout << a[n];
+	return 0;
+}
