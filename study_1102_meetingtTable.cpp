ebe49283715282b091588cd22a6b00d30c20e7d9
benlee73 <55429965+benlee73@users.noreply.github.com>
2019-11-06 19:18:44 +0900

    Add files via upload

---
+++ study_1102_meetingtTable.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>		// 백준 1931
+#include <algorithm>
+using namespace std;
+
+struct timetable {
+	int begin, end;
+};
+
+bool comp(const timetable& a, const timetable& b) {
+	if (a.end == b.end)
+		return a.begin < b.begin;		// 종료시간이 같다면, 시작시간 순
+	else
+		return a.end < b.end;			// 종료시간이 다르면, 종료시간 순
+}
+
+int main()
+{
+	timetable m[100000];
+	int n, cnt(0), last(0), x(0);
+
+	cin >> n;
+	
+	for (int i = 0; i < n; i++)
+		cin >> m[i].begin >> m[i].end;
+
+	sort(m, m+n, comp);
+	
+	for (int i = 0; i < n; i++)
+	{
+		if (last <= m[i].begin) {
+			last = m[i].end;
+			cnt++;
+		}
+	}
+	cout << cnt;;
+	return 0;
+}
