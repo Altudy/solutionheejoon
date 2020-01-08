d06a7a4f4db7b7ca246de61ee53e749b31ff9fe1
benlee73 <55429965+benlee73@users.noreply.github.com>
2020-01-08 17:44:44 +0900

    Add files via upload

---
+++ 백준/practice.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+
+	int n;
+	int three(0), five(0);
+
+	cin >> n;
+
+	while (1)
+	{
+		if (n % 5 == 0) break;
+		n -= 3;
+		three++;
+	}
+
+	if (n < 0) {
+		cout << "-1";
+		return 0;
+	}
+
+	five = n / 5;
+	cout << three + five;
+
+	return 0;
+}
