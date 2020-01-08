8b23d09515e17cb1ffbd04956bfc0deaf594204f
benlee73 <55429965+benlee73@users.noreply.github.com>
2020-01-08 17:33:38 +0900

    Add files via upload

---
+++ 백준/practice.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>		//백준 16112
+#include <algorithm>
+using namespace std;
+
+void input();
+int n, k;
+long long int arr[300001];
+long long int ex(0);
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin >> n >> k;
+	input();
+
+	sort(arr, arr + n);
+
+	for (int i = 0; i < n; i++)
+	{
+		ex += min(i, k) * arr[i];
+	}
+	cout << ex;
+
+	return 0;
+}
+
+void input()
+{
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+}
