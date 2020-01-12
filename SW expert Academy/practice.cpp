2b7ab51f1b0ba20fbebd97cd261231535700b711
benlee73 <55429965+benlee73@users.noreply.github.com>
2020-01-13 01:16:34 +0900

    Add files via upload

---
+++ SW expert Academy/practice.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int T, n, m, ans(0);
+int snack[1000];
+
+int main(int argc, char** argv)
+{
+	cin >> T;
+
+	for (int test_case = 1; test_case <= T; ++test_case)
+	{
+		cin >> n >> m;
+
+		for (int i = 0; i < n; i++)
+			cin >> snack[i];
+
+		sort(snack, snack+n);
+
+		for (int i = 0; i < n - 1; i++)
+		{
+			if (snack[n - 1 - i] >= m) continue;
+			for (int j = 1; j < n - i; j++)
+			{
+				if (snack[n - 1 - i] + snack[n - 1 - i - j] > m) continue;
+				else if (snack[n - 1 - i] + snack[n - 1 - i - j] == m)
+				{
+					i = n; j = n; ans = m; break;
+				}
+				ans = max(ans, snack[n - 1 - i] + snack[n - 1 - i - j]);
+			}
+		}
+		if (ans == 0) cout << "#" << test_case << " -1" << endl;
+		else cout << "#" << test_case << " " << ans << endl;
+		ans = 0;
+	}
+	return 0;
+}
