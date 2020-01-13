1148b6e0f354376ba0669fc4c4d826b289d10fe3
benlee73 <55429965+benlee73@users.noreply.github.com>
2020-01-13 11:21:13 +0900

    Add files via upload

---
+++ SW expert Academy/practice.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	ios::sync_with_stdio(false);
+	int T;
+	cin >> T;
+	long long int k;
+
+	for (int test_case = 1; test_case <= T; ++test_case)
+	{
+		cin >> k;
+		cout << "#" << test_case << " " << 2 * (k-1) * (k-1) + 1 << " " << 2 * k * k - 1 << endl;
+	}
+	return 0;
+}
