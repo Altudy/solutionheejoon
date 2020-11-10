affff1db95800dae611402a73b9f66d6e1ce2c7c
benlee73 <55429965+benlee73@users.noreply.github.com>
2020-11-10 16:47:36 +0900

    Update 3 Friendly Number 구하기.cpp

--- Debug&Run/3 Friendly Number 구하기.cpp
+++ Debug&Run/3 Friendly Number 구하기.cpp
@@ -1,55 +1,55 @@
-	#include <iostream>
-	#include <vector>
-	#include <algorithm>
-	#include <string>
-	#include <cstdlib>
-	#include <math.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cstdlib>
+#include <math.h>
 
-	using namespace std;
+using namespace std;
 
-	int main()
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	int n, temp;
+	vector<int> v;
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
 	{
-		ios::sync_with_stdio(false);
-		cin.tie(0); cout.tie(0);
+		cin >> temp;
+		v.push_back(temp);
+	}
 
-		int n, temp;
-		vector<int> v;
+	vector<bool> chk;
+	chk.assign(n, false);
 
-		cin >> n;
-		for (int i = 0; i < n; i++)
-		{
-			cin >> temp;
-			v.push_back(temp);
+	int a;
+
+	for (int i = 1; i < n; i++)
+	{
+		a = abs(v[i] - v[i - 1]);
+		if (chk[a] == true) {
+			cout << "NFN"; return 0;
 		}
+		chk[a] = true;
+	}
 
-		vector<bool> chk;
-		chk.assign(n, false);
+	bool fn=true;
 
-		int a;
-
-		for (int i = 1; i < n; i++)
-		{
-			a = abs(v[i] - v[i - 1]);
-			if (chk[a] == true) {
+	for (int i = 1; i < n; i++)
+	{
+		if (chk[i] == false) {
+			fn = false;
+		}
+		else
+			if (!fn)
+			{
 				cout << "NFN"; return 0;
 			}
-			chk[a] = true;
-		}
-
-		bool fn=true;
-
-		for (int i = 1; i < n; i++)
-		{
-			if (chk[i] == false) {
-				fn = false;
-			}
-			else
-				if (!fn)
-				{
-					cout << "NFN"; return 0;
-				}
-		}
-		cout << "FN";
-
-		return 0;
 	}
+	cout << "FN";
+
+	return 0;
+}
