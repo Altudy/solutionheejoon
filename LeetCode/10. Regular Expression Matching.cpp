040556d813c800c4a8c33173169d0a1b22758b7b
benlee73 <55429965+benlee73@users.noreply.github.com>
2020-12-22 17:19:10 +0900

    #Hard #String #DP #Backtracking

--- LeetCode/10. Regular Expression Matching.cpp
+++ LeetCode/10. Regular Expression Matching.cpp
@@ -54,3 +54,39 @@ private:
         return false;
     }
 };
+///////////////////////////////////////////////////////////////////////////////
+/* best solution
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        bool OPT[s.length() + 1][p.length() + 1];
+        for(int i = 0; i < s.length() + 1; i++){
+            for(int j = 0; j < p.length() + 1; j++)
+                OPT[i][j] = false;
+        }
+        OPT[0][0] = true;
+        for (int i = 0; i < p.length(); i++) {
+            if (p[i] == '*' && OPT[0][i - 1]) {
+                OPT[0][i+1] = true;
+            }
+        }
+        for (int i = 0 ; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p[j] == '.')
+                    OPT[i+1][j+1] = OPT[i][j];
+                if (p[j] == s[i]) 
+                    OPT[i+1][j+1] = OPT[i][j];
+                if (p[j] == '*') {
+                    if (p[j-1] != s[i] && p[j-1] != '.')
+                        OPT[i+1][j+1] = OPT[i+1][j-1];
+                    else
+                        OPT[i+1][j+1] = (OPT[i+1][j] || OPT[i][j+1] || OPT[i+1][j-1]);
+                }
+            }
+        }
+        return OPT[s.length()][p.length()];
+    }
+};
+
+*/
