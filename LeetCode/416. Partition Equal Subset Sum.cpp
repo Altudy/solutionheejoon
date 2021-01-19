263e5fa6c8fe4fcfee1a8be7e680f338ea22217c
benlee73 <55429965+benlee73@users.noreply.github.com>
2021-01-19 12:26:17 +0900

    #Medium #DP #knapsack #bitset

--- LeetCode/416. Partition Equal Subset Sum.cpp
+++ LeetCode/416. Partition Equal Subset Sum.cpp
@@ -66,3 +66,16 @@ public:
         return !(sum & 1) && bits[sum >> 1];
     }
 };
+
+/////////////////////////////////////////////////////////////////////
+// best solution 3
+
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        bitset<10001> bits(1);
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        for (auto n : nums) bits |= bits << n;
+        return !(sum & 1) && bits[sum >> 1];
+    }
+};
