dd69865dbf8a3e72a32d95b52d91354b5b61419b
benlee73 <55429965+benlee73@users.noreply.github.com>
2021-01-22 16:00:20 +0900

    #Medium #String #Backtracking #DFS #Recursion

--- LeetCode/17. Letter Combinations of a Phone Number.cpp
+++ LeetCode/17. Letter Combinations of a Phone Number.cpp
@@ -3,7 +3,7 @@ public:
     vector<string> letterCombinations(string digits) {
         d = digits;
         len = digits.size();
-        if (!len) return ans;
+        if(!len) return ans;
         string s;
         addletter(s, len);
         return ans;
@@ -16,12 +16,26 @@ public:
             return;
         }
 
-        int n = d[len - l] - '0';
+        int n = d[len - l] - 48;
 
         for (int i = 0; i < dial[n].size(); i++)
         {
-            s += dial[n][i];
+            s.push_back(dial[n][i]);
             addletter(s, l-1);
             s.pop_back();
         }
     }
+
+private:
+    string d;
+    int len;
+    vector<string> ans;
+    vector<vector<char>> dial{ {}, {}, {'a','b','c'},
+                              {'d','e','f'},
+                              {'g','h','i'},
+                              {'j','k','l'},
+                              {'m','n','o'},
+                              {'p','q','r','s'},
+                              {'t','u','v'},
+                              {'w','x','y','z'}};
+};
